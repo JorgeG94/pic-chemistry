b7bb7e89568b40b3580a8eedc93acd65231a0523
JorgeG94 <JorgeG94@users.noreply.github.com>
2026-01-13 09:50:26 +0000

    deploy: d93c44a5e8a35b14e128ea211faf986bf6690b02

--- src/mqc_config_adapter.f90
+++ src/mqc_config_adapter.f90
@@ -43,6 +43,9 @@ module mqc_config_adapter
       type(hessian_keywords_t) :: hessian  !! Hessian calculation keywords
       type(aimd_keywords_t) :: aimd        !! AIMD calculation keywords
       type(scf_keywords_t) :: scf          !! SCF calculation keywords
+
+      ! Output control
+      logical :: skip_json_output = .false.  !! Skip JSON output for large calculations
    end type driver_config_t
 
 contains
@@ -118,6 +121,9 @@ contains
       driver_config%aimd%output_frequency = mqc_config%aimd_output_frequency
       driver_config%scf%max_iterations = mqc_config%scf_maxiter
       driver_config%scf%convergence_threshold = mqc_config%scf_tolerance
+
+      ! Output control
+      driver_config%skip_json_output = mqc_config%skip_json_output
 
    end subroutine config_to_driver
 
